45439515a501af261128555755c288ef141cfb60
havodaxe <havodaxe@gmail.com>
2021-06-22 18:12:04 +0200

    Rewrite fragment shader

--- fragment_main.glsl
+++ fragment_main.glsl
@@ -4,7 +4,6 @@ out vec4 outputColor;
 
 void main()
 {
-	float lerpValue = gl_FragCoord.y / 500.0f;
-
-	outputColor = mix(vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(0.0f, 1.0f, 0.0f, 1.0f), lerpValue);
+	vec2 normCoords = gl_FragCoord.xy / 500.0f;
+	outputColor = vec4(normCoords, 0.5f, 1.0f);
 }
